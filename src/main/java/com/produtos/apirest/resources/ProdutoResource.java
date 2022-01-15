a3408a1f563c8a4b36c5ba6bad198bbffb84a40c
Jusley Souza <jusleysouza31@gmail.com>
2022-01-15 15:33:07 -0300

    implementação do método Put

--- src/main/java/com/produtos/apirest/resources/ProdutoResource.java
+++ src/main/java/com/produtos/apirest/resources/ProdutoResource.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,23 +22,28 @@ public class ProdutoResource {
 	@Autowired  //importa o repository
 	ProdutoRepository produtoRepository;
 	
-	@GetMapping("/produtos")  //lista todos os produtos salvos no banco de dados
+	@GetMapping("/produtos")  //lista todos os produtos salvos no bd
 	public List<Produto> listaProdutos(){
 		return produtoRepository.findAll();
 	}
 	
-	@GetMapping("/produto/{id}")  //lista produto especifico salvo no banco de dados atraves do id
+	@GetMapping("/produto/{id}")  //lista um produto especifico salvo no bd atraves do id
 	public Produto listaProdutoUnico(@PathVariable(value="id")long id){
 		return produtoRepository.findById(id);
 	}
 	
-	@PostMapping("/produto")
+	@PostMapping("/produto") //salva um produto no bd
 	public  Produto salvaProduto(@RequestBody Produto produto) {
 		return produtoRepository.save(produto);
 	}
 	
-	@DeleteMapping("/produto")
+	@DeleteMapping("/produto") //deleta um produto no bd
 	public void deletaProduto(@RequestBody Produto produto) {
 		produtoRepository.delete(produto);
+	}
+	
+	@PutMapping("/produto") //atualiza um produto no bd
+	public Produto atualizaProduto(@RequestBody Produto produto) {
+		return produtoRepository.save(produto);
 	}
 }
