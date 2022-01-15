5f25af33c74feb97d1248bd9c1e236a89d60462b
Jusley Souza <jusleysouza31@gmail.com>
2022-01-15 14:46:20 -0300

    Criação da classe controller (resources) e dos métodos GetMapping (findAll e findById) e PostMapping e modificação no GenerationType

--- src/main/java/com/produtos/apirest/models/Produto.java
+++ src/main/java/com/produtos/apirest/models/Produto.java
@@ -16,7 +16,7 @@ public class Produto implements Serializable {
 	private static final long serialVersionUID = 1l;
 	
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private long id;
 	
 	private String nome;
