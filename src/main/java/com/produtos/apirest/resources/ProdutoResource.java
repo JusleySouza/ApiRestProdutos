6fbc94d49947f5a8e177bec8b3f9f859b81870a3
Jusley Souza <jusleysouza31@gmail.com>
2022-01-15 15:17:46 -0300

    Implemantação do método delete

--- src/main/java/com/produtos/apirest/resources/ProdutoResource.java
+++ src/main/java/com/produtos/apirest/resources/ProdutoResource.java
@@ -3,6 +3,7 @@ package com.produtos.apirest.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -31,7 +32,12 @@ public class ProdutoResource {
 	}
 	
 	@PostMapping("/produto")
-	public Produto salvaProduto(@RequestBody Produto produto) {
+	public  Produto salvaProduto(@RequestBody Produto produto) {
 		return produtoRepository.save(produto);
+	}
+	
+	@DeleteMapping("/produto")
+	public void deletaProduto(@RequestBody Produto produto) {
+		produtoRepository.delete(produto);
 	}
 }
